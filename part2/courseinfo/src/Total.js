797125cf3561deb0ac3f0285adff887b2b4fa75d
Anna Riazantseva <anyariazantzeva@yandex.ru>
2020-10-25 16:39:48 +0200

    Completed exercises part2 2.1.-2.5.

---
+++ part2/courseinfo/src/Total.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Total = (props) => {
+  const total = props.parts.reduce((s, p) => {
+    return s + p.exercises;
+  }, 0);
+
+  return <strong>Total number of exercises {total}</strong>;
+};
+
+export default Total;
