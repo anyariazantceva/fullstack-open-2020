e42b8fa19fdd12f4e6b7d31bc02232f7c7295cb5
Anna Riazantseva <anyariazantzeva@yandex.ru>
2021-04-06 20:43:13 +0300

    Exercises 4.1-4.2

---
+++ part4/models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const uniqueValidator = require("mongoose-unique-validator");
+const config = require("../utils/config");
+const logger = require("../utils/logger");
+
+const blogSchema = new Schema({
+  title: { type: String, minlength: 4, required: true, unique: true },
+  author: { type: String, minlength: 4, required: true, unique: true },
+  url: { type: String, minlength: 4, required: true, unique: true },
+  likes: { type: Number, required: true },
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+blogSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Blog", blogSchema);
