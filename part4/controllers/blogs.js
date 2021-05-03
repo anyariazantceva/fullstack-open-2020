8b1f15aa5ec5762725b4d07eb3be0519917b4cfe
Anna Riazantseva <anyariazantzeva@yandex.ru>
2021-05-03 21:08:32 +0300

    Exercises 4.20-4.22

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,5 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response, next) => {
   const blogs = await Blog.find({});
@@ -9,14 +19,23 @@ blogsRouter.get("/", async (request, response, next) => {
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = request.user;
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.json(savedBlog);
 });
 
@@ -30,8 +49,19 @@ blogsRouter.get("/:id", async (request, response, next) => {
 });
 
 blogsRouter.delete("/:id", async (request, response, next) => {
-  await Blog.findByIdAndRemove(request.params.id);
-  response.status(204).end();
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  const { id } = request.params;
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = request.user;
+  const blog = await Blog.findById(request.params.id);
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.findByIdAndRemove(id);
+    response.status(204).end();
+  } else {
+    response.status(401).json({ error: "You can't delete this blog" });
+  }
 });
 
 blogsRouter.put("/:id", (request, response, next) => {
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -4,9 +4,13 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const blogSchema = new Schema({
   title: { type: String, minlength: 4, required: true, unique: true },
-  author: { type: String, minlength: 4, required: true, unique: true },
-  url: { type: String, minlength: 4, required: true, unique: true },
+  author: { type: String, minlength: 4, required: true },
+  url: { type: String, minlength: 4, required: true },
   likes: { type: Number, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 blogSchema.set("toJSON", {
